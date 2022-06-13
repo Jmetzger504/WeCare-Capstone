41a9be0801f3c16332030d94ee42ce0af6eac227
GrandmaCoding <jmetzger5@gmail.com>
2022-06-13 09:50:58 -0500

    Generated Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Navigation.module.css';
+
+const Navigation = () => (
+  <div className={styles.Navigation}>
+    Navigation Component
+  </div>
+);
+
+Navigation.propTypes = {};
+
+Navigation.defaultProps = {};
+
+export default Navigation;
