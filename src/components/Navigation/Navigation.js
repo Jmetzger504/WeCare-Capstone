843a234d477b9a1b7a2c52244c599723d01c03f5
GrandmaCoding <jmetzger5@gmail.com>
2022-06-14 12:49:54 -0500

    Right aligned Navbar items!

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Navigation.module.css';
 import { Navbar,Nav,NavDropdown,Container } from 'react-bootstrap';
 import { IconContext } from 'react-icons';
 import {BsTelephoneFill} from "react-icons/bs"
 import { useEffect } from 'react';
+import './Navigation.css';
 
 function Navigation() {
   
   
   return  (
     <Navbar bg="dark" expand="lg" variant="dark">
-    <Container>
+    <Container fluid>
       <Navbar.Brand href="#home">WeCare</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="container-fluid">
-          <IconContext.Provider value={{ color: "white",
-                                          style: { verticalAlign: 'middle' } }}>
-            
-            <Navbar.Brand className="ms-auto">
+      <Navbar.Collapse id="basic-navbar-nav" className = "justify-content-end">
+        <Nav className = "justify-content-end">
+          <Nav.Link to = "/" className = "me-3 navlink">Profile</Nav.Link>
+          <Nav.Link to = "/" className = "me-3 navlink">My Appointments</Nav.Link>
+          <IconContext.Provider value={{ color: "white"}}>
+            <Navbar.Brand >
               <BsTelephoneFill/>
               Call Us: +1 (504)-123-4567
             </Navbar.Brand>
