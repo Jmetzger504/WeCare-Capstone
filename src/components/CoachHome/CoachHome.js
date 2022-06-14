1ab337a69f4d3328b5b45f841aa3adface92c762
GrandmaCoding <jmetzger5@gmail.com>
2022-06-14 11:27:02 -0500

    user and coach home pages generated

---
+++ src/components/CoachHome/CoachHome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './CoachHome.module.css';
+
+const CoachHome = () => (
+  <div className={styles.CoachHome}>
+    CoachHome Component
+  </div>
+);
+
+CoachHome.propTypes = {};
+
+CoachHome.defaultProps = {};
+
+export default CoachHome;
---
+++ src/components/UserHome/UserHome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './UserHome.module.css';
+
+const UserHome = () => (
+  <div className={styles.UserHome}>
+    UserHome Component
+  </div>
+);
+
+UserHome.propTypes = {};
+
+UserHome.defaultProps = {};
+
+export default UserHome;
