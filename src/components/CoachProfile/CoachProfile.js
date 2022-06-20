493cbb35741e4fbe0f53af3f8c7a84f1ff1aca4e
GrandmaCoding <jmetzger5@gmail.com>
2022-06-20 10:35:25 -0500

    Generated user and coach profile components

---
+++ src/components/CoachProfile/CoachProfile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './CoachProfile.module.css';
+
+const CoachProfile = () => (
+  <div className={styles.CoachProfile}>
+    CoachProfile Component
+  </div>
+);
+
+CoachProfile.propTypes = {};
+
+CoachProfile.defaultProps = {};
+
+export default CoachProfile;
---
+++ src/components/UserProfile/UserProfile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './UserProfile.module.css';
+
+const UserProfile = () => (
+  <div className={styles.UserProfile}>
+    UserProfile Component
+  </div>
+);
+
+UserProfile.propTypes = {};
+
+UserProfile.defaultProps = {};
+
+export default UserProfile;
