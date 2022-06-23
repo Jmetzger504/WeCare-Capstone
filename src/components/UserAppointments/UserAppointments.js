4a685f8cdd4936ccfef4be082d6648ecd449f643
GrandmaCoding <jmetzger5@gmail.com>
2022-06-23 11:36:34 -0500

    Cancel appointment modal renders appropriately

--- src/components/UserAppointments/UserAppointments.js
+++ src/components/UserAppointments/UserAppointments.js
@@ -4,7 +4,7 @@ import styles from './UserAppointments.module.css';
 import { store } from '../../stores/store';
 import {useEffect,useState} from 'react'
 import Navigation from '../Navigation/Navigation';
-import { Button } from 'react-bootstrap';
+import { Modal,Button } from 'react-bootstrap';
 import axios from 'axios';
 
 const UserAppointments = () => {
@@ -12,7 +12,11 @@ const UserAppointments = () => {
   let state = store.getState();
   const [myBookings,setMyBookings] = useState([]);
   const [isLoading,setLoading] = useState(true);
+  const [isRescheduling,setIsRescheduling] = useState(false);
+  const [isCancelling,setIsCancelling] = useState(false);
+  const [showCancelModal, setShowCancelModal] = useState(false);
 
+  //Set background and get current appointments.
   useEffect(() => {
     document.body.style.background = "url('/assets/Images/cloud-2725520_960_720.jpg') no-repeat center center fixed";
     document.body.style.backgroundSize = "cover";
@@ -27,6 +31,7 @@ const UserAppointments = () => {
     })
   },[]);
 
+  //Re-renders after axios request completes.
   useEffect(() => {},[isLoading]);
 
   if(isLoading) {
@@ -45,14 +50,32 @@ const UserAppointments = () => {
         <Navigation/>
         {myBookings.map((myBooking) => {
           return (
-            <div className = "container p-3 mt-5 bg-dark text-white"
-            style = {{"width": "20%", "borderRadius": "2.5%", "textAlign": "center"}}>
-              <h1>Appointment Date</h1>
-              <h2>{myBooking.appointmentDate}</h2>
-              <h3 className = "mb-5">Slot: {myBooking.slot}</h3>
-              <Button variant = "info" className = "mt-5 mb-3 text-white">Reschedule your Appointment</Button>
-              <Button variant = "danger" className = "mb-3">Cancel your Appointment</Button>
-            </div>
+            <>
+              <div className = "container p-3 mt-5 bg-dark text-white"
+              style = {{"width": "20%", "borderRadius": "2.5%", "textAlign": "center"}}>
+                <h1>Appointment Date</h1>
+                <h2>{myBooking.appointmentDate}</h2>
+                <h3 className = "mb-5">Slot: {myBooking.slot}</h3>
+                <Button variant = "info" className = "mt-5 mb-3 text-white">Reschedule your Appointment</Button>
+                <Button
+                  variant = "danger" 
+                  className = "mb-3"
+                  data-toggle="modal"
+                  onClick = {() => setShowCancelModal(true)}>Cancel your Appointment
+                </Button>
+              </div>
+              <Modal centered show={showCancelModal} onHide={() => setShowCancelModal(false)}>
+                <Modal.Body>Are you sure you need to cancel the appointment?</Modal.Body>
+                <Modal.Footer>
+                  <Button className = "mx-auto" style = {{"width": "40%"}} variant="success">
+                    Yes
+                  </Button>
+                  <Button className = "mx-auto" style = {{"width": "40%"}} variant="danger" onClick={() => setShowCancelModal(false)}>
+                    No
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </>
           )
         })}
       </>
